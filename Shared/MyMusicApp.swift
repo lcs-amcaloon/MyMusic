7ac0776dc1faefa7dc86c960c3dae1374018f728
Angus McAloon <amcaloon@lcs.on.ca>
2022-03-25 11:24:46 -0400

    Initial Commit

---
+++ Shared/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  Shared
+//
+//  Created by Angus McAloon on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
