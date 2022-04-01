4b92f023fb0e52add5f627b5a727df1db14eb4a1
Angus McAloon <amcaloon@lcs.on.ca>
2022-04-01 14:06:45 -0400

    started working on implementing a search filter

---
+++ Shared/Model/ResultVisibility.swift
@@ -0,0 +1,24 @@
+//
+//  ResultVisibility.swift
+//  MyMusic (iOS)
+//
+//  Created by Angus McAloon on 2022-04-01.
+//
+
+import Foundation
+
+enum ResultVisibility: String {
+    
+    case all = "all"
+    case one = "1/10"
+    case two = "2/10"
+    case three = "3/10"
+    case four = "4/10"
+    case five = "5/10"
+    case six = "6/10"
+    case seven = "7/10"
+    case eight = "8/10"
+    case nine = "9/10"
+    case ten = "10/10"
+    
+}
--- Shared/Views/ListenedAlbums.swift
+++ Shared/Views/ListenedAlbums.swift
@@ -2,7 +2,7 @@
 //  ListenedAlbums.swift
 //  MyMusic (iOS)
 //
-//  Created by Angus McAloon on 2022-03-30.
+//  Created on 2022-03-30.
 //
 
 import SwiftUI
@@ -15,23 +15,48 @@ struct ListenedAlbums: View {
     
     @State var showingCompletedAlbums = true
     
+    @State var results: [Album] = []
+    
+    @State private var selectedResultVisibility: ResultVisibility = .all
+    
     var body: some View {
         NavigationView {
-            List {
+            
+            VStack {
+                    Picker("Filter", selection: $selectedResultVisibility) {
+                        Text(ResultVisibility.all.rawValue)
+                            .tag(ResultVisibility.all)
+                        Text(ResultVisibility.one.rawValue)
+                            .tag(ResultVisibility.one)
+                        Text(ResultVisibility.two.rawValue)
+                            .tag(ResultVisibility.two)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding(.horizontal)
+            }
+            .padding(.bottom)
+            
+            List(filter(results, by: selectedResultVisibility)) { result in
                 
                 ForEach(store.albums) { album in
+                    
+                    
                     
                     if showingCompletedAlbums {
                         
                         if album.AlbumRating > 0 {
                             Album_Cell(album: album, triggerListUpdate: .constant(true))
+                            
                         }
-
+                        
                     }
                     
                 }
                 .onDelete(perform: store.deleteAlbums)
                 .onMove(perform: store.moveAlbums)
+                
+                
+                
             }
             .navigationTitle("Listened")
             .toolbar {
@@ -55,6 +80,28 @@ struct ListenedAlbums: View {
                 EditButton()
             }
         }
+        
+    }
+    func filter(_ listOfResults: [Album], by visibilty: ResultVisibility) -> [Album] {
+        
+        if visibilty == .all {
+            return listOfResults
+        } else {
+            
+            var filteredResults: [Album] = []
+            
+            for currentResult in listOfResults {
+                
+                if visibilty == .one && currentResult.AlbumRating < 2 {
+                    filteredResults.insert(currentResult, at: 0)
+                }
+                
+            }
+            
+            return filteredResults
+            
+        }
+            
     }
 }
 
